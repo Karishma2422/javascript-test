b49ad94a6b63bca7df0c22277ab1dbe1d8a52bce
Karishma2422 <karishmapatil74@gmail.com>
2024-10-21 18:10:31 +0530

    java question

---
+++ day 21/10/24/index.js
@@ -0,0 +1,162 @@
+//qusetion 1 print numbers from 10-20 with 
+// for (var i = 10; i <= 20; i++) {
+//     console.log(i); // 1
+//   }
+//   console.log( "this are no. between 10-20");
+
+
+// //ques no.2 print even num  between 1-10
+// for(var i =0; i<=10; i++){
+//     if (i % 2==0){
+//         console.log(i)
+//     }
+// }
+// console.log("above are even numbers");
+
+// //question 3 print sum 1-5
+
+// var i = 1;
+// var j = 2;
+// var k = 3;
+// var l = 4;
+// var m = 5;
+// {
+//     console.log(i+j+k+l+m );
+//     console.log("Addition of numbers 1-5")
+// }
+
+
+
+//question 4 print reverse no. from 5-1
+// for(var i=5; i>=1; i--){
+//     console.log(i);
+// }
+// console.log("this is reverse loop")
+
+//question 5 check no postive or negative
+// function findNumber() {
+//         var number1 = -3;
+//         var number2 = 0;
+//         if (number1 < number2) {
+//           console.log("Number is less than 0 its negative ");
+//         } else {
+//           console.log( "Number is greter than 0 its positive.");
+//         }
+//       }
+//       findNumber();
+
+//question 7 print largets numbers of two
+// function largestNumber() {
+//         var number1 = 5;
+//         var number2 = 9;
+//         if (number1 > number2) {
+//           console.log("Number 1 is greater than Number 2");
+//         } else(number1 < number2) {
+//           console.log( "Number 2 is greter than number 1.");
+//         }
+//       }
+//       largestNumber();
+
+
+
+
+// question 8 number is even or odd 
+// var array =[2,6,7,8] ;
+
+// if (array  % 2== 1){
+// console.log(array[]);
+// }
+
+  
+
+//question 9 print numbres 1 to n.
+// var i = 0;
+// var n = 4;
+
+// for(i = 1; i <=n; i++){
+//         console.log(i)
+//     }
+
+//question 10 sum of two no. 
+// var i = 3;
+// var j = 8;
+// {
+//     console.log(i+j)
+// }
+ 
+
+//question 11 print 1-5 no.
+// var i = 0;
+// for(i = 1; i <=5; i++){
+//     console.log(i)
+// }
+
+
+//question 12 number is gretar than 10
+// function GreaterNumber() {
+//     var number1 = 10;
+//     var number2 = 12;
+//     if (number1 > number2) {
+//       console.log("Number 10 is greater than Number 12");
+//     } else {
+//       console.log( " YES ,Number 12 is greter than number 10.");
+//     }
+//   }
+//   GreaterNumber();
+  
+//qyestion 13 print first 4 odd numbers
+
+
+
+
+
+//question 14 print produvt of two num.
+// var number1 = 4;
+// var number2 = 5;
+// function multipilcation(value1, value2) {
+//   console.log("product  value is" ,value1 * value2 );
+// }
+// multipilcation(number2, number1);
+
+//question 15 check number is zero.
+// function checkNumber() {
+//         var num1 = 0;
+        
+//         if (num1 ) {
+//           console.log("Num is not zero");
+//         } else {
+//           console.log( " YES ,0.");
+//         }
+//       }
+//       checkNumber();
+
+//question 16
+
+// 17 print sum of first 3 positive numbers 
+// var number1 = 1;
+// var number2 = 2;
+// var number3 = 3;
+// function Addition(value1, value2, value3) {
+//   console.log("sum of three positive numbers " ,value1 + value2 +value3);
+// }
+// Addition(number1, number2, number3);
+
+//quetion 18 check numbers less than 100
+
+
+
+//question 19 diffrence between two num.
+
+// var number1 = 10;
+// var number2 = 4;
+// function subtraction(value1, value2) {
+//   console.log("subraction value is" ,value1 - value2 );
+// }
+// subtraction(number1, number2);
+
+
+//question 20 print no 1 to 10 
+
+// for(var i=1; i<=10; i++){
+//         console.log(i);
+//     }
